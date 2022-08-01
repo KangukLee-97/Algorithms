db07914a382b6280179b97101e2d889ef82133d0
KangukLee-97 <rkddnrdl97@naver.com>
2022-08-02 00:04:05 +0900

    [이취코] 구현-상하좌우

---
+++ 이취코/구현/상하좌우/상하좌우.py
@@ -0,0 +1,24 @@
+from shutil import move
+
+
+n = int(input())
+plan = list(input().split())
+x, y = 1, 1
+
+dx = [ 0, 0, -1, 1 ]
+dy = [ -1, 1, 0, 0 ]
+move_types = [ 'L', 'R', 'U', 'D' ]
+
+for p in plan:
+    for i in range(len(move_types)):
+        if p == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+        
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    else:
+        x = nx
+        y = ny
+
+print(x, y)
